8d78a27fbefff61b4ea6d465b429156c9af4fc68
LV_Embedded <nguyenlamvien0110@gmail.com>
2020-07-04 20:06:12 +0700

    update main.c

--- Src/main.c
+++ Src/main.c
@@ -2,14 +2,18 @@
 #include "I2C.h"
 #include "BH1750.h"
 #include "Systick.h"
-
+#include "DS18B20.h"
+#include "Timerx.h"
 uint16_t lux;
+float Water_Temperature;
 int main()
 {    
 	Systick_Configuration();
-    I2C_Config();
+    //I2C_Config();
+    TIM6_Config();
     while (1)
     {
-        lux=GetLightIntensity();
+        //lux=GetLightIntensity();
+        Water_Temperature=GetWaterTemp();
     }
 }
