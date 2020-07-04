192ad22ec48fedf3190a4b7472b8dc0e9920df5e
LV_Embedded <nguyenlamvien0110@gmail.com>
2020-07-04 15:10:11 +0700

    add GPIO function to project

---
+++ Mylib/GPIO.c
@@ -0,0 +1,38 @@
+/* 
+* July 04, 2020
+* LV_Embedded
+ */
+
+#include "GPIO.h"
+
+/**
+ * 
+ * @param GPIOx - GPIO Port want to set
+ * @param GPIO_Pin - GPIO Pin want to set
+ * @param Set_Pin_Output - Set GPIO_Pin of GPIOx PORT mode OUTPUT
+ **/
+
+void Set_Pin_Output(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode=GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_Pin=GPIO_Pin;
+    GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+    GPIO_Init(GPIOx,&GPIO_InitStructure);
+}
+
+/**
+ * 
+ * @param GPIOx - GPIO Port want to set
+ * @param GPIO_Pin - GPIO Pin want to set
+ * @param Set_Pin_Input - Set GPIO_Pin of GPIOx PORT mode OUTPUT
+ **/
+
+void Set_Pin_Input(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_Pin=GPIO_Pin;
+    GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_UP;
+    GPIO_Init(GPIOx,&GPIO_InitStructure);
+}
---
+++ Mylib/GPIO.h
@@ -0,0 +1,9 @@
+/* 
+* July 04, 2020
+* LV_Embedded
+ */
+
+#include "../Src/stm32f4xx.h"
+
+void Set_Pin_OutPut(GPIO_TypeDef *GPIOx,uint16_t GPIO_Pin);
+void Set_Pin_Input(GPIO_TypeDef *GPIOx,uint16_t GPIO_Pin);
