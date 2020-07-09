5015665d24b33f3dc5108a78b383dd2cd958d89b
LV_Embedded <nguyenlamvien0110@gmail.com>
2020-07-09 17:53:48 +0700

    update EC_Sensor file

---
+++ Mylib/EC_Sensor.c
@@ -0,0 +1,10 @@
+/** 
+ * 
+ * July 9, 2020
+ * LV_Embedded
+ * nguyenlamvien0110@gmail.com 
+ * https://github.com/vien20010
+ * 
+ **/
+
+#include "EC_Sensor.h"
---
+++ Mylib/EC_Sensor.h
@@ -0,0 +1,16 @@
+/** 
+ * 
+ * July 9, 2020
+ * LV_Embedded
+ * nguyenlamvien0110@gmail.com 
+ * https://github.com/vien20010
+ * 
+ **/
+
+#ifndef _EC_SENSOR_H
+#define _EC_SENSOR_H
+
+#include "../Src/stm32f4xx.h"
+
+void EC_Sensor_Config(void);
+#endif
