af96f39de0171610b2f18f012a8cbd6272f327a9
LV_Embedded <nguyenlamvien0110@gmail.com>
2020-07-09 17:47:18 +0700

    update all keil project

--- Src/main.c
+++ Src/main.c
@@ -1,4 +1,4 @@
-#include "stm32f4xx.h"                  // Device header
+#include "stm32f4xx.h" // Device header
 #include "I2C.h"
 #include "BH1750.h"
 #include "Systick.h"
@@ -7,13 +7,13 @@
 uint16_t lux;
 float Water_Temperature;
 int main()
-{    
-	Systick_Configuration();
-    //I2C_Config();
+{
+    Systick_Configuration();
+    I2C_Config();
     TIM6_Config();
     while (1)
     {
-        //lux=GetLightIntensity();
-        Water_Temperature=GetWaterTemp();
+        lux=GetLightIntensity();
+        Water_Temperature = GetWaterTemp();
     }
 }
